d8e6bbead57b8c733355913e95b8af044eb18761
STROMTVX <127620276+STROMTVX@users.noreply.github.com>
2023-03-11 21:33:08 +0200

    Create new line2

--- HomeWork2/HomeWork2/Program.cs
+++ HomeWork2/HomeWork2/Program.cs
@@ -8,6 +8,7 @@ namespace HomeWork2
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World again!");
+            Console.WriteLine("Hello World again again!");
         }
     }
 }
