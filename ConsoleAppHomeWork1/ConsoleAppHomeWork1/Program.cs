0a391cd1284b78e43a6ab2ca25d79f99b66060f2
STROMTVX <127620276+STROMTVX@users.noreply.github.com>
2023-03-11 21:20:51 +0200

    created new project

---
+++ ConsoleAppHomeWork1/ConsoleAppHomeWork1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppHomeWork1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
